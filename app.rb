ed20ba7ca5f5e98dc50f40ef7fcd9d97c1c2996b
Graham Jarvis <zerothousand111@proton.me>
2025-08-20 19:57:02 +0100

    created helper methods, get methods and user.erb for basic user pages

--- app.rb
+++ app.rb
@@ -8,11 +8,45 @@ before do
   @users = @user_file.each_with_object([]) { |user, array| array << user[0].to_s } 
 end
 
+# helper methods
+
+# retrieve email of user
+def fetch_email(name)
+  @user_file[name.downcase.to_sym][:email]
+end
+
+# retrieve interests of user
+def fetch_interests(name)
+  @user_file[name.downcase.to_sym][:interests].join(", ")
+end
+
+# get methods
+
 # create redirect to home or a custom 404?
 
 get "/" do
-
   erb :home
 end
 
-# helper methods
+# can I programatically have just 1 get method for all users???
+get "/jamy" do
+  @name = "Jamy"
+  @email = fetch_email(@name)
+  @interests = fetch_interests(@name)
+  erb :user
+end
+
+get "/nora" do
+  @name = "Nora"
+  @email = fetch_email(@name)
+  @interests = fetch_interests(@name)
+  erb :user
+end
+
+get "/hiroko" do
+  @name = "Hiroko"
+  @email = fetch_email(@name)
+  @interests = fetch_interests(@name)
+  erb :user
+end
+
