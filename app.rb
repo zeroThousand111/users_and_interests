c8b12a10efdbeebc902d366b2d5a77a33618790b
Graham Jarvis <zeroThousand111@proton.me>
2025-08-21 12:12:38 +0100

    make get method programatic, add other users to user page and creating total users and interest in layout

--- app.rb
+++ app.rb
@@ -5,48 +5,58 @@ require 'yaml'
 
 before do
   @user_file = YAML.load_file("data/users.yaml")
-  @users = @user_file.each_with_object([]) { |user, array| array << user[0].to_s } 
+  @users = @user_file.each_with_object([]) { |user, array| array << user[0].to_s }
 end
 
 # helper methods
 
-# retrieve email of user
+## retrieve email of user
 def fetch_email(name)
   @user_file[name.downcase.to_sym][:email]
 end
 
-# retrieve interests of user
+## retrieve interests of user
 def fetch_interests(name)
   @user_file[name.downcase.to_sym][:interests].join(", ")
 end
 
-# get methods
+## identify other users
+def id_other_users(name)
+  output = []
+  @users.each { |user| output << user if user != name }
+  output
+end
 
-# create redirect to home or a custom 404?
+## count total users and interests
+def count_users
+  @users.size
+end
+
+def count_interests
+  total = 0
+
+  @user_file.each do |user, hash|
+    total += hash[:interests].size
+  end
+
+  total
+end
+
+# get methods
 
 get "/" do
   erb :home
 end
 
-# can I programatically have just 1 get method for all users???
-get "/jamy" do
-  @name = "Jamy"
-  @email = fetch_email(@name)
-  @interests = fetch_interests(@name)
-  erb :user
-end
+get "/:name" do
+  @name = params[:name]
 
-get "/nora" do
-  @name = "Nora"
-  @email = fetch_email(@name)
-  @interests = fetch_interests(@name)
-  erb :user
+  if @users.include?(@name)
+    @email = fetch_email(@name)
+    @interests = fetch_interests(@name)
+    @others = id_other_users(@name)
+    erb :user
+  else
+    erb :user_not_found
+  end
 end
-
-get "/hiroko" do
-  @name = "Hiroko"
-  @email = fetch_email(@name)
-  @interests = fetch_interests(@name)
-  erb :user
-end
-
